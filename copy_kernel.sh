0273ad130f2a65b0c22c60c3df3c08d56ff28f05
Wes Hampson <WesleyHampson@gmail.com>
2018-10-10 13:30:20 -0500

    Add license header

--- copy_kernel.sh
+++ copy_kernel.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Copyright (C) 2018 Wes Hampson. All Rights Reserved.                         #
+#                                                                              #
+# This is free software: you can redistribute it and/or modify                 #
+# it under the terms of version 2 of the GNU General Public License            #
+# as published by the Free Software Foundation.                                #
+#                                                                              #
+# See LICENSE in the top-level directory for a copy of the license.            #
+# You may also visit <https://www.gnu.org/licenses/gpl-2.0.txt>.               #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+#-------------------------------------------------------------------------------
+#   File: copy_kernel.sh
+# Author: Wes Hampson
+#------------------------------------------------------------------------------#
+
 MOUNT_DIR=/tmp/ext2img
 DISK_IMG=floppy.img
 KERNEL_IMG=kernel
--- kernel.c
+++ kernel.c
@@ -1,3 +1,19 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+ * Copyright (C) 2018 Wes Hampson. All Rights Reserved.                       *
+ *                                                                            *
+ * This is free software: you can redistribute it and/or modify               *
+ * it under the terms of version 2 of the GNU General Public License          *
+ * as published by the Free Software Foundation.                              *
+ *                                                                            *
+ * See LICENSE in the top-level directory for a copy of the license.          *
+ * You may also visit <https://www.gnu.org/licenses/gpl-2.0.txt>.             *
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*-----------------------------------------------------------------------------
+ *   File: kernel.c
+ * Author: Wes Hampson
+ *----------------------------------------------------------------------------*/
+
 __asm__ (".code16gcc\n");
 
 void print(char *s)
